c93f4f7250c04eb1b1b59cb1049832efb34a2409
Aleksey Samoshin <aleksey.samoshin@yandex.ru>
2022-11-06 17:22:17 +0400

    feature: добавлены поля жанра и рейтинка в класс Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,6 +17,8 @@ public class Film {
     private LocalDate releaseDate;
     private Integer duration;
     private List<User> likes;
+    private Set<Genre> genre;
+    private RatingMpa rating;
 
     public Film() {
         likes = new ArrayList<>();
